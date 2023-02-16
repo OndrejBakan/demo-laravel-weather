bf5c675d7906574b0c3ecc4c19ebe1cf528d332a
Ondrej Bakan <ondrej@bakan.cz>
2023-02-16 06:40:37 +0100

    WIP, time to sleep.

---
+++ resources/views/weather/index.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html>
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="https://cdn.tailwindcss.com"></script>
+</head>
+
+<body>
+    <div class="flex items-center justify-center h-screen">
+        <div class="bg-indigo-500 p-6 rounded shadow-2xl">
+            <input type="text" class="rounded p-2 h-10">
+            <button class="rounded p-2 bg-white- h-10">
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+                    <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z" />
+                </svg>
+            </button>
+        </div>
+    </div>
+</body>
+
+</html>
