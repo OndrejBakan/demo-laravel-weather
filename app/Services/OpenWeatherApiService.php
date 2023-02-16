bb886f061221d200009c94be25a39eecb532b74f
Ondrej Bakan <ondrej@bakan.cz>
2023-02-16 06:51:56 +0100

    Working prototype.

--- app/Services/OpenWeatherApiService.php
+++ app/Services/OpenWeatherApiService.php
@@ -2,10 +2,23 @@
 
 namespace App\Services;
 
+use Illuminate\Support\Facades\Http;
+
 class OpenWeatherApiService
 {
-    public function getWeatherForCity($city) : array
+    public $apiKey;
+
+    public function __construct()
     {
-        return ['city' => $city, 'message' => 'Zima.'];
+        $this->apiKey = env('OPEN_WEATHER_API_KEY');
+    }
+
+    public function getWeatherForCity($city)
+    {
+        $url = sprintf('http://api.openweathermap.org/data/2.5/weather?q=%s&APPID=%s', $city, $this->apiKey);
+
+        $response = Http::get($url);
+        
+        return $response->json();
     }
 }
